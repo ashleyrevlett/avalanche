8237253bc432dfb1c6c21e07abb729611c3fba8e
ashley revlett <ashleyrevlett@gmail.com>
2023-11-17 13:28:46 -0500

    Add spawner generation

--- scripts/terrain.gd
+++ scripts/terrain.gd
@@ -11,18 +11,15 @@ var ground_objects = []
 
 var camera_size: Vector2
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	camera_size = get_viewport_rect().size # * camera.zoom
 	rotation_degrees = angle
 	for x in range(1, 4):
 		_spawn_ground()	
-	# get visible camera rect
-	# spawn series of ground objects in a line, starting from the bottom left corner of rect
 
 
 func _spawn_ground():
-	print("spawned ground!")
 	var instance = ground_scene.instantiate()
 	if (ground_objects.size() == 0):
 		instance.position = Vector2(-180, 370)
@@ -34,10 +31,8 @@ func _spawn_ground():
 		instance.position = new_pos
 	ground_objects.append(instance)
 	add_child(instance)
-	print("ground_objects:", ground_objects)
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# when the first ground goes offscreen, remove it and add a new one
 	# get global pos of instance, compare to camera rect
@@ -45,11 +40,7 @@ func _process(delta):
 		var first_pos = ground_objects[0].global_position
 		var camera_rect = Rect2(camera.get_screen_center_position() - camera_size / 2, camera_size)
 		if (first_pos.x + ground_width/2 < camera_rect.position.x - ground_width/2):
-			print("offscreen!")
 			var instance = ground_objects.pop_front()
 			instance.queue_free()
-			print("removed ground!")
 			_spawn_ground()
 	
-	# when the camera comes within X of the end of the existing ground objects, 
-	# spawn some more

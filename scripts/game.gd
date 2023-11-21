5a8446cf956171849feadb85f2de0fd65a57adb8
ashley revlett <ashleyrevlett@gmail.com>
2023-11-21 11:11:52 -0500

    Add UI theme, pause menu

--- scripts/game.gd
+++ scripts/game.gd
@@ -8,12 +8,33 @@ var altitude: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_tree().paused = true
+	%GameStartUI.show()
 	%GameOverUI.hide()
-	%GameOverUI.process_mode = Node.PROCESS_MODE_ALWAYS
-	%RestartButton.button_up.connect(_restart_game)
-	%Character.player_death.connect(_on_game_over)
-	get_tree().paused = false
+	%PauseUI.hide()
 	
+	%StartButton.button_up.connect(_start_game)
+	%GameOverRestartButton.button_up.connect(_restart_game)
+	%QuitButton.button_up.connect(_restart_game)
+	%ResumeButton.button_up.connect(_unpause_game)
+	%Character.player_death.connect(_on_game_over)
+
+
+func _start_game():
+	print("Starting!")
+	%GameStartUI.hide()
+	get_tree().paused = false
+
+
+func _pause_game():
+	get_tree().paused = true
+	%PauseUI.show()
+	
+	
+func _unpause_game():
+	get_tree().paused = false
+	%PauseUI.hide()
+
 
 func _on_game_over():
 	%GameOverLabel.text = "Game Over!\n Final Altitude: %sm" % floor(altitude)
@@ -28,3 +49,6 @@ func _restart_game():
 func _process(delta):
 	altitude = max(0, camera.position.y / 20 * -1)
 	score_label.text = "Altitude: %sm" % floor(altitude)
+	
+	if (Input.is_action_just_pressed("pause")):
+		_pause_game()

56316fe8a6473e35e6291ad17138f69618a23fc1
ashley revlett <ashleyrevlett@gmail.com>
2023-11-29 11:26:50 -0500

    Add UI focus

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,11 +7,15 @@ extends Node2D
 var altitude: float
 var high_score: int
 
+enum State {TITLE, GAMEPLAY, PAUSE, GAMEOVER}
+var state: State
+
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	get_tree().paused = true
-	
+func _ready():	
+	state = State.TITLE
+	%StartButton.grab_focus()
 	%StartButton.button_up.connect(_start_game)
+	
 	%GameOverRestartButton.button_up.connect(_restart_game)
 	%QuitButton.button_up.connect(_restart_game)
 	%ResumeButton.button_up.connect(_unpause_game)
@@ -23,24 +27,31 @@ func _ready():
 	else:
 		%HighScoreLabel.hide()
 
+	get_tree().paused = true
+
 
 func _start_game():
+	state = State.GAMEPLAY
 	get_tree().paused = false
 	%GameStartUI.hide()
 	%ScoreUI.show()
 
 
 func _pause_game():
+	state = State.PAUSE
 	get_tree().paused = true
 	%PauseUI.show()
+	%ResumeButton.grab_focus()
 	
 	
 func _unpause_game():
+	state = State.GAMEPLAY
 	get_tree().paused = false
 	%PauseUI.hide()
 
 
 func _on_game_over():
+	state = State.GAMEOVER
 	%GameOverLabel.text = "Final Altitude: %sm" % floor(altitude)
 	
 	if altitude >= high_score:
@@ -48,19 +59,22 @@ func _on_game_over():
 		save_high_score(altitude)
 
 	%GameOverUI.show()
+	%GameOverRestartButton.grab_focus()
 	get_tree().paused = true
 	
 
 func _restart_game():
+	state = State.TITLE
 	var _reload = get_tree().reload_current_scene()
 
 
 func _process(delta):
-	altitude = max(0, camera.position.y / 20 * -1)
-	score_label.text = "Altitude: %sm" % floor(altitude)
-	
-	if (Input.is_action_just_pressed("pause")):
-		_pause_game()
+	if state == State.GAMEPLAY:
+		altitude = max(0, camera.position.y / 20 * -1)
+		score_label.text = "Altitude: %sm" % floor(altitude)
+		
+		if (Input.is_action_just_pressed("pause")):
+			_pause_game()
 
 
 func save_high_score(score):
